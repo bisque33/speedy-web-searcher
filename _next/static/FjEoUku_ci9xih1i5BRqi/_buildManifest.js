81dbe264b80e48dd37ca0d10232f708e51a521f1
bisque33 <bisque33@users.noreply.github.com>
2022-06-21 13:51:12 +0000

    deploy: 102270385c7efee720bf0d37051cbefa123d8b29

--- _next/static/FjEoUku_ci9xih1i5BRqi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cf35bc11072831f2.css","static/chunks/pages/index-7cd4ea436aad6e47.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
